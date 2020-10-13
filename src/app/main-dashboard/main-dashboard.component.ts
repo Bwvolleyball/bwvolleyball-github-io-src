b08a03a27e0538c820c61707f50bf5ea4e501d46
Brandon Ward <brandon.ward1993@gmail.com>
2020-10-12 21:58:09 -0600

    Fleshing out Resume Highlights card (#20)

--- src/app/main-dashboard/main-dashboard.component.ts
+++ src/app/main-dashboard/main-dashboard.component.ts
@@ -5,6 +5,7 @@ import {TwitterFeedComponent} from '../twitter-feed/twitter-feed.component';
 import {WipCardComponent} from '../wip-card/wip-card.component';
 import {LinkedinBadgeComponent} from '../linkedin-badge/linkedin-badge.component';
 import {MediumFeedComponent} from '../medium-feed/medium-feed.component';
+import {ResumeComponent} from '../resume/resume.component';
 
 @Component({
   selector: 'app-main-dashboard',
@@ -17,14 +18,14 @@ export class MainDashboardComponent {
   twitterFeedComponent = TwitterFeedComponent;
   linkedinBadgeComponent = LinkedinBadgeComponent;
   mediumFeedComponent = MediumFeedComponent;
-  wipCardComponent = WipCardComponent;
+  resumeComponent = ResumeComponent;
 
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({matches}) => {
       if (matches) {
         return [
-          {title: 'Resume', cols: 2, rows: 1, content: this.wipCardComponent},
+          {title: 'Resume Highlights', cols: 2, rows: 1, content: this.resumeComponent},
           {title: 'Blogs', cols: 2, rows: 1, content: this.mediumFeedComponent},
           {title: 'Twitter', cols: 2, rows: 1, content: this.twitterFeedComponent},
           {title: 'LinkedIn', cols: 2, rows: 1, content: this.linkedinBadgeComponent}
@@ -32,7 +33,7 @@ export class MainDashboardComponent {
       }
 
       return [
-        {title: 'Resume', cols: 2, rows: 1, content: this.wipCardComponent},
+        {title: 'Resume Highlights', cols: 2, rows: 1, content: this.resumeComponent},
         {title: 'Blogs', cols: 1, rows: 1, content: this.mediumFeedComponent},
         {title: 'Twitter', cols: 1, rows: 2, content: this.twitterFeedComponent},
         {title: 'LinkedIn', cols: 1, rows: 1, content: this.linkedinBadgeComponent}
